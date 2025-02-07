a103386083dd4c3c499e7c5225f9400c91aa99bb
choo779325 <choo779325@sas.edu.sg>
2025-02-07 21:03:54 +0800

    coral pivot code 😛

--- src/main/java/frc/robot/subsystems/superstructure/CoralPivot.java
+++ src/main/java/frc/robot/subsystems/superstructure/CoralPivot.java
@@ -4,18 +4,111 @@
 
 package frc.robot.subsystems.superstructure;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static edu.wpi.first.units.Units.Volt;
+
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.utils.FSMSubsystem;
+import frc.utils.TalonFXConfigurator;
 
 public class CoralPivot extends FSMSubsystem {
   /** Creates a new CoralPivot. */
-  public CoralPivot() {
 
+  //Constants
+
+  private static final int CORAL_PIVOT_ID = 0;
+  private static final double kP = 0.1;
+  private static final double kI = 0.0;
+  private static final double kD = 0.0;
+  private static final double kS = 0.0;
+  private static final double kV = 0.12;
+  private static final double kA = 0.0;
+  private static final double kG = 0.0;
+  private static final double CORAL_PIVOT_GEAR_RATIO = 0;
+  private static final double mmAcceleration = 0;
+  private static final double mmCruiseVelocity = 0;
+  private static final double mmJerk = 0;
+  private static final double CORAL_PIVOT_DOCKED_POS = 0.0;
+  private static final double CORAL_PIVOT_HUMAN_PLAYER_POS = 0.0;
+  private static final double CORAL_PIVOT_REEF_POS = 0.0;
+  private static final double CORAL_PIVOT_ALLOWED_ERROR = 0;
+
+  private TalonFX m_coralPivotMotor;
+
+  private MotionMagicVoltage m_motionMagicVoltage = new MotionMagicVoltage(0).withSlot(0);
+    
+  private NeutralModeValue m_currentNeutralMode = NeutralModeValue.Brake; //what is this
+
+  public CoralPivot() {
+    setName("Coral Pivot");
+    m_coralPivotMotor = new TalonFX(CORAL_PIVOT_ID, "rio");
+    motorConfigurations();
+  }
+
+  private void motorConfigurations() {
+    TalonFXConfigurator.configureTalonFX(
+      m_coralPivotMotor,
+      "KrakenX60",
+      m_currentNeutralMode,
+      InvertedValue.Clockwise_Positive, //CHECK
+      kP, kI, kD, kS, kV, kA, kG,
+      CORAL_PIVOT_GEAR_RATIO,
+      mmAcceleration,
+      mmCruiseVelocity,
+      mmJerk
+      );
+  }
+
+  private void setControl(TalonFX motor, ControlRequest req) {
+      if (motor.isAlive()) {
+        motor.setControl(req);
+      }
+  }
+
+  public void resetCoralPivotPosition() {
+    m_coralPivotMotor.setPosition(0);
   }
 
   private void toggleIdleMode(){
-    
+    m_currentNeutralMode = (m_currentNeutralMode == NeutralModeValue.Brake) 
+        ? NeutralModeValue.Coast 
+        : NeutralModeValue.Brake;
+    motorConfigurations(); //why are you configuring motor here
+    SmartDashboard.putString("Coral Pivot Idle Mode", m_currentNeutralMode.toString());
   }
+
+  private final SysIdRoutine elevatorCharacterization = new SysIdRoutine(
+    new SysIdRoutine.Config(null, Voltage.ofBaseUnits(3, Volt), null),
+    new SysIdRoutine.Mechanism(
+        (Voltage volts) -> {
+            m_coralPivotMotor.setVoltage(volts.in(Volt));
+        },
+        null,
+        this
+    )
+    );
+    
+  public Command elevatorSysIDQuasistatic(SysIdRoutine.Direction direction) {
+    return elevatorCharacterization.quasistatic(direction);
+  }
+
+  public Command elevatorSysIDDynamic(SysIdRoutine.Direction direction) {
+    return elevatorCharacterization.dynamic(direction);
+  }
+
+  public boolean isCoralPivotAtGoal() {
+    return Math.abs(m_coralPivotMotor.getPosition().getValueAsDouble() - 
+        ((CoralPivotStates)getCurrentState()).getSetpointValue()) < CORAL_PIVOT_ALLOWED_ERROR;
+  }
+
 
   @Override
   public void periodic() {
@@ -24,13 +117,13 @@ public class CoralPivot extends FSMSubsystem {
 
   @Override
   protected void enterNewState() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'enterNewState'");
+    CoralPivotStates newState = (CoralPivotStates)getCurrentState();
+    setControl(m_coralPivotMotor, m_motionMagicVoltage.withPosition(newState.getSetpointValue()));
   }
 
   @Override
   protected void exitCurrentState() {
-    // TODO Auto-generated method stub
+    // is there anything i need to add for this
     throw new UnsupportedOperationException("Unimplemented method 'exitCurrentState'");
   }
 
@@ -38,7 +131,7 @@ public class CoralPivot extends FSMSubsystem {
 
   @Override
   protected void executeCurrentStateBehavior() {
-    // TODO Auto-generated method stub
+    // is there anything i need to add for this
     throw new UnsupportedOperationException("Unimplemented method 'executeCurrentStateBehavior'");
   }
 
@@ -46,31 +139,43 @@ public class CoralPivot extends FSMSubsystem {
 
   @Override
   public void stop() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    m_coralPivotMotor.stopMotor();
   }
 
 
 
   @Override
   public boolean isInState(Enum<?> state) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isInState'");
+    return getCurrentState() == state;
   }
 
 
 
   @Override
   public Enum<?> getCurrentState() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getCurrentState'");
+    return currentState;
   }
 
 
 
   @Override
   protected Enum<?>[] getStates() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getStates'");
+    return CoralPivotStates.values();
   }
+
+  public enum CoralPivotStates {
+    CORAL_PIVOT_DOCKED(CORAL_PIVOT_DOCKED_POS),
+    CORAL_PIVOT_HUMAN_PLAYER(CORAL_PIVOT_HUMAN_PLAYER_POS),
+    CORAL_PIVOT_REEF(CORAL_PIVOT_REEF_POS);
+
+    private final double setpointValue;
+
+    CoralPivotStates(double setpointValue) {
+        this.setpointValue = setpointValue;
+    }
+
+    public double getSetpointValue() {
+        return setpointValue;
+    }
+}
 }
